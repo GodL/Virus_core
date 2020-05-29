720e794abf2039865eb206b7f80443ebd8073049
lihao10 <lihao10@staff.sina.com.cn>
2020-05-29 10:17:52 +0800

    create handler

---
+++ Example/Example/Virus_core/VCSqlite3Handler.c
@@ -0,0 +1,9 @@
+//
+//  VCSqlite3Handler.c
+//  Example
+//
+//  Created by lihao10 on 2020/5/29.
+//  Copyright © 2020 GodL. All rights reserved.
+//
+
+#include "VCSqlite3Handler.h"
---
+++ Example/Example/Virus_core/VCSqlite3Handler.h
@@ -0,0 +1,14 @@
+//
+//  VCSqlite3Handler.h
+//  Example
+//
+//  Created by lihao10 on 2020/5/29.
+//  Copyright © 2020 GodL. All rights reserved.
+//
+
+#ifndef VCSqlite3Handler_h
+#define VCSqlite3Handler_h
+
+#include <stdio.h>
+
+#endif /* VCSqlite3Handler_h */
